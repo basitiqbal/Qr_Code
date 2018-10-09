58588521b05de0784794e86c789d5ca7183db230
BASIT <b2016cs127@gmail.com>
2018-10-09 18:13:11 +0500

    Initial commit

---
+++ app/src/main/java/com/example/hussain/myapplication/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.hussain.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity
+{
+    private Button scan_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        scan_btn = (Button) findViewById(R.id.scan_btn);
+        final Activity activity = this;
+        scan_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator intentIntegrator  = new IntentIntegrator(activity);
+                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                intentIntegrator.setPrompt("SCAN");
+                intentIntegrator.setCameraId(0);
+                intentIntegrator.setBeepEnabled(false);
+                intentIntegrator.setBarcodeImageEnabled(false);
+                intentIntegrator.initiateScan();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null)
+        {
+            if(result.getContents() == null)
+            {
+                Toast.makeText(this , "You cancel this scanner ",Toast.LENGTH_LONG).show();
+            }
+            else
+                Toast.makeText(this , result.getContents() , Toast.LENGTH_SHORT).show();
+
+        } 
+
+else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
+
